64a82bcd65c2ddef3d46a68c0c4b323217d6a7ab
CarlosP112 <c.poveda@outlook.com>
2022-07-24 11:09:25 -0600

    font bug

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,13 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-      <link
-      rel="preload"
-      href="/fonts/StarJedi-DGRW.ttf"
-      as="font"
-      type="font/woff"
-      crossOrigin=""
-      />
       <link rel="shortcut icon" href="/star-wars.ico"/>
       </Head>
       <body>
