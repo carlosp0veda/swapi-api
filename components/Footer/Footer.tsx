e44124646af28a59309cc13539a948fa8ee3bf6b
CarlosP112 <c.poveda@outlook.com>
2022-07-24 11:29:49 -0600

    footer links

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -8,19 +8,19 @@ const Footer = () => {
         <SocialLinks />
         <p className={styles.FooterText}>TM & © Lucasfilm Ltd. All Rights Reserved</p>
         <div className={styles.FooterLinks}>
-          <a className={styles.FooterLink} href="/" target="_blank">
+          <a className={styles.FooterLink} href="http://disneytermsofuse.com/" rel="noreferrer" target="_blank">
             Terms of Use
           </a>
-          <a className={styles.FooterLink} href="/" target="_blank">
+          <a className={styles.FooterLink} href="http://disney.go.com/guestservices/legalnotices" rel="noreferrer" target="_blank">
             Additional Content Information
           </a>
-          <a className={styles.FooterLink} href="/" target="_blank">
+          <a className={styles.FooterLink} href="http://disneyprivacycenter.com/" rel="noreferrer" target="_blank">
             Privacy Policy
           </a>
-          <a className={styles.FooterLink} href="/" target="_blank">
+          <a className={styles.FooterLink} href="https://www.shopdisney.com/franchises/star-wars/?CMP=SYN-Dcom&att=StarWars_Footer_Store" rel="noreferrer" target="_blank">
             Star Wars at shopDisney
           </a>
-          <a className={styles.FooterLink} href="/" target="_blank">
+          <a className={styles.FooterLink} href="https://support.starwars.com/" rel="noreferrer" target="_blank">
             Star Wars Helpdesk
           </a>
         </div>
